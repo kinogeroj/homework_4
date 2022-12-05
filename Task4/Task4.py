85f335d6e834cbc4f7a6743aa97205cf54632694
Dmitrii Mironov <dimamironov@mail.ru>
2022-12-05 14:45:31 +0300

    Task 4 completed

--- Task4/Task4.py
+++ Task4/Task4.py
@@ -4,10 +4,74 @@
 # - k=3 => 2*x^3 + 4*x^2 + 4*x + 5
 
 import os
+import random
 
 os.system('cls')
 
-print('Переведем десятичное число в двоичное.')
+print('Создадим многочлен степени k и запишем его в файл, коэффициенты будут сгенерированы случайным образом автоматически.')
 
-n = int(input('Введите десятичное число: '))
+print()
 
+k = int(input('Введите степень многочлена (k): '))
+
+coeflist = []
+
+for i in range(0, k + 1):
+    
+    n = random.randint(-10, 10)
+    
+    coeflist.append(n)
+
+print(f'Получили следующие коэффициенты для многочлена: {coeflist}.')
+
+def createpoly(coeflist, var_string = 'x'):
+
+    """Фунцкия принимает на вход список коэффициентов, на выходе получаем строку с многочленом."""
+
+    resstr = ''
+    
+    first_pow = len(coeflist) - 1
+    
+    for i, coef in enumerate(coeflist):
+        
+        power = first_pow - i
+
+        if coef:
+            
+            if coef < 0:
+
+                sign, coef = (' - ' if resstr else '- '), -coef
+
+            elif coef > 0:
+
+                sign = (' + ' if resstr else '')
+
+            str_coef = '' if coef == 1 and power != 0 else str(coef)
+
+            if power == 0:
+
+                str_power = ''
+
+            elif power == 1:
+
+                str_power = var_string
+
+            else:
+
+                str_power = var_string + '^' + str(power)
+
+            resstr += sign + str_coef + str_power
+
+    return resstr
+
+with open('file1.txt', 'w') as file:
+    file.write(createpoly(coeflist))
+
+print()
+
+print('Задача выполнена, ниже представлено содержимое файла file1.txt:')
+
+with open('file1.txt', 'r') as file:
+    result = file.read()
+
+print(result)
